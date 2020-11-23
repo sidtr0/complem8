fe117a4f9dec158616405721995f9b927ac63273
DracTheDino <sidtrip06@gmail.com>
2020-11-23 09:32:05 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const hexRgb = require('hex-rgb');
+const rgbHex = require('rgb-hex')
+const limit = 255;
+var args = process.argv.slice(2);
+
+// console.log(hexRgb(args[0]));
+
+const colourToConvert = hexRgb(args[0]);
+
+var red = limit - colourToConvert.red;
+var blue = limit - colourToConvert.blue;
+var green = limit - colourToConvert.green;
+
+console.log("Complementary:", rgbHex(red, blue, green))
